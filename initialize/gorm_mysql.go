0a933f0c7353fcd3156c1a3315f2954a6f7045a7
daihao4371 <daihao_4371@163.com>
2025-03-31 20:22:51 +0800

    fix: 修复连接MySQL失败panic报错

--- initialize/gorm_mysql.go
+++ initialize/gorm_mysql.go
@@ -4,7 +4,9 @@ import (
 	"KubeGale/config"
 	"KubeGale/global"
 	"KubeGale/initialize/internal"
+
 	_ "github.com/go-sql-driver/mysql"
+	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -13,14 +15,18 @@ import (
 func GormMysql() *gorm.DB {
 	m := global.KUBEGALE_CONFIG.Mysql
 	if m.Dbname == "" {
+		global.KUBEGALE_LOG.Warn("未配置MySQL数据库名称，跳过数据库初始化")
 		return nil
 	}
+	
 	mysqlConfig := mysql.Config{
 		DSN:                       m.Dsn(), // DSN system source name
 		DefaultStringSize:         191,     // string 类型字段的默认长度
 		SkipInitializeWithVersion: false,   // 根据版本自动配置
 	}
+	
 	if db, err := gorm.Open(mysql.New(mysqlConfig), internal.Gorm.Config(m.Prefix, m.Singular)); err != nil {
+		global.KUBEGALE_LOG.Error("MySQL连接失败", zap.Error(err))
 		return nil
 	} else {
 		db.InstanceSet("gorm:table_options", "ENGINE="+m.Engine)
--- middleware/casbin_rbac.go
+++ middleware/casbin_rbac.go
@@ -1,20 +1,31 @@
 package middleware
 
 import (
+	"KubeGale/global"
 	"github.com/casbin/casbin/v2"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
-	"log"
 )
 
 func InitCasbin(db *gorm.DB) *casbin.Enforcer {
+	// 检查数据库连接是否为空
+	if db == nil {
+		global.KUBEGALE_LOG.Warn("数据库连接为空，Casbin 初始化跳过")
+		return nil
+	}
+
 	adapter, err := gormadapter.NewAdapterByDB(db)
 	if err != nil {
-		log.Fatalf("Failed to create adapter: %v", err)
+		global.KUBEGALE_LOG.Error("Casbin适配器初始化失败", zap.Error(err))
+		return nil
 	}
-	enforcer, err := casbin.NewEnforcer("./model.conf", adapter)
+	
+	e, err := casbin.NewEnforcer("model.conf", adapter)
 	if err != nil {
-		log.Fatalf("Failed to create enforcer: %v", err)
+		global.KUBEGALE_LOG.Error("Casbin初始化失败", zap.Error(err))
+		return nil
 	}
-	return enforcer
+	
+	return e
 }
