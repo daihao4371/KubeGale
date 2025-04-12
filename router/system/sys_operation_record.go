9d266190e51ae66178d708e5cae7d6019e478d44
daihao4371 <daihao_4371@163.com>
2025-04-12 22:46:50 +0800

    fix: 修复操作记录用户不显示

--- router/system/sys_operation_record.go
+++ router/system/sys_operation_record.go
@@ -11,8 +11,8 @@ func (s *OperationRecordRouter) InitSysOperationRecordRouter(Router *gin.RouterG
 		operationRecordRouter.DELETE("deleteSysOperationRecord", operationRecordApi.DeleteSysOperationRecord)           // 删除SysOperationRecord
 		operationRecordRouter.DELETE("deleteSysOperationRecordByIds", operationRecordApi.DeleteSysOperationRecordByIds) // 批量删除SysOperationRecord
 		// 在路由配置文件中添加或修改路由
-		operationRecordRouter.GET("/findSysOperationRecord/:id", operationRecordApi.FindSysOperationRecord) // 通过ID查询操作记录
-		operationRecordRouter.GET("getSysOperationRecordList", operationRecordApi.GetSysOperationRecordList)            // 获取SysOperationRecord列表
+		operationRecordRouter.GET("findSysOperationRecord/:id", operationRecordApi.FindSysOperationRecord)   // 通过ID查询操作记录
+		operationRecordRouter.GET("getSysOperationRecordList", operationRecordApi.GetSysOperationRecordList) // 获取SysOperationRecord列表
 
 	}
 }
