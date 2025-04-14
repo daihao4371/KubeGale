e42317ea59a3b1980457355579b729d0b5ed5992
daihao4371 <daihao_4371@163.com>
2025-04-14 22:10:59 +0800

    feat: 新增部分代码注释

--- initialize/internal/gorm.go
+++ initialize/internal/gorm.go
@@ -19,16 +19,17 @@ type _gorm struct{}
 func (g *_gorm) Config(prefix string, singular bool) *gorm.Config {
 	var general config.GeneralDB
 	general = global.KUBEGALE_CONFIG.Mysql.GeneralDB
-	
+
 	// 创建自定义日志记录器
 	logWriter := NewWriter(general, log.New(os.Stdout, "\r\n", log.LstdFlags))
-	
+
 	// 配置日志级别
 	logLevel := general.LogLevel()
+	// 如果需要根据配置文件决定，可以取消注释下面的代码
 	if general.LogMode == "debug" || general.LogMode == "Debug" {
-		logLevel = logger.Info // 设置为Info以显示SQL语句
+		logLevel = logger.Info // 设置为Info以显示SQL语句 // 强制设置为 Error 级别，只记录错误
 	}
-	
+
 	return &gorm.Config{
 		Logger: logger.New(logWriter, logger.Config{
 			SlowThreshold: 200 * time.Millisecond,
