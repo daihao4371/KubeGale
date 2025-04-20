2572b168bc6044cefbd83fb9e7dbcf9d48af7c33
daihao4371 <daihao_4371@163.com>
2025-04-20 22:54:48 +0800

    优化中间件

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"KubeGale/global"
-	"KubeGale/model/system"
 	"KubeGale/utils"
 	"errors"
 	"github.com/golang-jwt/jwt/v4"
@@ -16,7 +15,6 @@ import (
 )
 
 var jwtService = service.ServiceGroupApp.SystemServiceGroup.JwtService
-var userService = service.ServiceGroupApp.SystemServiceGroup.UserService
 
 func JWTAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -52,11 +50,11 @@ func JWTAuth() gin.HandlerFunc {
 		// 已登录用户被管理员禁用 需要使该用户的jwt失效 此处比较消耗性能 如果需要 请自行打开
 		// 用户被删除的逻辑 需要优化 此处比较消耗性能 如果需要 请自行打开
 
-		if user, err := userService.FindUserByUuid(claims.UUID.String()); err != nil || user.Enable == 2 {
-			_ = jwtService.JsonInBlacklist(system.JwtBlacklist{Jwt: token})
-			response.FailWithDetailed(gin.H{"reload": true}, err.Error(), c)
-			c.Abort()
-		}
+		//if user, err := userService.FindUserByUuid(claims.UUID.String()); err != nil || user.Enable == 2 {
+		//	_ = jwtService.JsonInBlacklist(system.JwtBlacklist{Jwt: token})
+		//	response.FailWithDetailed(gin.H{"reload": true}, err.Error(), c)
+		//	c.Abort()
+		//}
 		c.Set("claims", claims)
 		if claims.ExpiresAt.Unix()-time.Now().Unix() < claims.BufferTime {
 			dr, _ := utils.ParseDuration(global.KUBEGALE_CONFIG.JWT.ExpiresTime)
