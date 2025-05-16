a1225631d7c74524e2d82b224b99503953873864
daihao4371 <daihao_4371@163.com>
2025-05-16 15:01:06 +0800

    dev(初始化路由逻辑完善)

--- router/cloudCmdb/cloud_load_balancer.go
+++ router/cloudCmdb/cloud_load_balancer.go
@@ -1,4 +1,25 @@
 package cloudCmdb
 
+import (
+	api "KubeGale/api/v1/cloudCmdb"
+	"KubeGale/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
 type CloudLoadBalancerRouter struct {
+}
+
+func (l *CloudLoadBalancerRouter) InitLoadBalancerRouter(Router *gin.RouterGroup) {
+	cloudLoadBalancerRouter := Router.Group("loadBalancer").Use(middleware.OperationRecord())
+	cloudLoadBalancerRouterWithoutRecord := Router.Group("loadBalancer")
+	cloudLoadBalancerApi := api.ApiGroupApp.CloudLoadBalancerApi
+	{
+		cloudLoadBalancerRouter.POST("sync", cloudLoadBalancerApi.CloudLoadBalancerSync) // 同步负载均衡
+		cloudLoadBalancerRouter.POST("tree", cloudLoadBalancerApi.CloudLoadBalancerTree) // 目录树
+	}
+
+	{
+		cloudLoadBalancerRouterWithoutRecord.POST("list", cloudLoadBalancerApi.CloudLoadBalancerList) // 分页获取列表
+	}
 }
--- router/cloudCmdb/cloud_rds.go
+++ router/cloudCmdb/cloud_rds.go
@@ -1,4 +1,23 @@
 package cloudCmdb
 
-type CloudRDSRouter struct {
+import (
+	api "KubeGale/api/v1/cloudCmdb"
+	"KubeGale/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+type CloudRDSRouter struct{}
+
+func (r *CloudRDSRouter) InitRDSRouter(Router *gin.RouterGroup) {
+	cloudrdsRouter := Router.Group("rds").Use(middleware.OperationRecord())
+	cloudrdsRouterWithoutRecord := Router.Group("rds")
+	cloudrdsApi := api.ApiGroupApp.CloudRDSApi
+	{
+		cloudrdsRouter.POST("sync", cloudrdsApi.CloudRDSSync) // 同步RDS
+		cloudrdsRouter.POST("tree", cloudrdsApi.CloudRDSTree) // 目录树
+	}
+
+	{
+		cloudrdsRouterWithoutRecord.POST("list", cloudrdsApi.CloudRDSList) // 分页获取列表
+	}
 }
