b9b9d86e233a3c8f4988ef850f2805c7dd9f9627
daihao4371 <daihao_4371@163.com>
2025-05-16 14:06:23 +0800

    dev(云平台区域管理)

--- api/v1/cloudCmdb/cloud_region.go
+++ api/v1/cloudCmdb/cloud_region.go
@@ -1,3 +1,30 @@
 package cloudCmdb
 
+import (
+	"KubeGale/global"
+	"KubeGale/model/common/request"
+	"KubeGale/model/common/response"
+	"KubeGale/utils"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
 type CloudRegionApi struct{}
+
+// CloudPlatformSyncRegion 同步云平台区域
+func (r *CloudRegionApi) CloudPlatformSyncRegion(c *gin.Context) {
+	var idInfo request.GetById
+	_ = c.ShouldBindJSON(&idInfo)
+	if err := utils.Verify(idInfo, utils.IdVerify); err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+
+	err := cloudRegionService.SyncRegion(idInfo.ID)
+	if err != nil {
+		global.KUBEGALE_LOG.Error("同步操作失败!", zap.Error(err))
+		response.FailWithMessage("同步操作失败", c)
+	} else {
+		response.OkWithMessage("同步操作成功, 数据异步处理中, 请稍后!", c)
+	}
+}
