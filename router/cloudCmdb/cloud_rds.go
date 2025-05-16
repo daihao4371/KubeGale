95383ee01e77b0dbe004337cc8881d052593d87f
daihao4371 <daihao_4371@163.com>
2025-05-16 15:04:00 +0800

    fix(修复代码命名规范问题)

--- router/cloudCmdb/cloud_rds.go
+++ router/cloudCmdb/cloud_rds.go
@@ -9,15 +9,15 @@ import (
 type CloudRDSRouter struct{}
 
 func (r *CloudRDSRouter) InitRDSRouter(Router *gin.RouterGroup) {
-	cloudrdsRouter := Router.Group("rds").Use(middleware.OperationRecord())
-	cloudrdsRouterWithoutRecord := Router.Group("rds")
-	cloudrdsApi := api.ApiGroupApp.CloudRDSApi
+	cloudRdsRouter := Router.Group("rds").Use(middleware.OperationRecord())
+	cloudRdsRouterWithoutRecord := Router.Group("rds")
+	cloudRdsApi := api.ApiGroupApp.CloudRDSApi
 	{
-		cloudrdsRouter.POST("sync", cloudrdsApi.CloudRDSSync) // 同步RDS
-		cloudrdsRouter.POST("tree", cloudrdsApi.CloudRDSTree) // 目录树
+		cloudRdsRouter.POST("sync", cloudRdsApi.CloudRDSSync) // 同步RDS
+		cloudRdsRouter.POST("tree", cloudRdsApi.CloudRDSTree) // 目录树
 	}
 
 	{
-		cloudrdsRouterWithoutRecord.POST("list", cloudrdsApi.CloudRDSList) // 分页获取列表
+		cloudRdsRouterWithoutRecord.POST("list", cloudRdsApi.CloudRDSList) // 分页获取列表
 	}
 }
